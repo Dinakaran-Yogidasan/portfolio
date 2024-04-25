dd026813dbb5c57adfa7ead4ea5f2a948b8a88e0
Yogidasan, Dinakaran (D.) <dannydina28@gmail.com>
2024-04-25 10:14:52 +0530

    preline-tailwind-css project Setup
    Adding theme for light and dark mode button

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-     
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <h1>Navbar</h1>
+      <input
+        data-hs-theme-switch=""
+        className="relative w-[3.25rem] h-7 bg-gray-500 checked:bg-none checked:bg-blue-600 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 ring-1 ring-transparent focus:border-gray-700 focus:ring-gray-700 focus:outline-none appearance-none
 
-export default App
+before:inline-block before:size-6 before:bg-white checked:before:bg-blue-200 before:translate-x-0 checked:before:translate-x-full before:shadow before:rounded-full before:transform before:ring-0 before:transition before:ease-in-out before:duration-200
+
+after:absolute after:end-1.5 after:top-[calc(50%-0.40625rem)] after:w-[.8125rem] after:h-[.8125rem] after:bg-no-repeat after:bg-[right_center] after:bg-[length:.8125em_.8125em] after:transform after:transition-all after:ease-in-out after:duration-200 after:opacity-70 checked:after:start-1.5 checked:after:end-auto"
+        type="checkbox"
+        id="darkSwitch"
+      ></input>
+    </>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// This code should be added to <head>.
+// It's used to prevent page load glitches.
+/** This Code is Used for dark mode */
+const html = document.querySelector("html");
+const isLightOrAuto =
+  localStorage.getItem("hs_theme") === "light" ||
+  (localStorage.getItem("hs_theme") === "auto" &&
+    !window.matchMedia("(prefers-color-scheme: dark)").matches);
+const isDarkOrAuto =
+  localStorage.getItem("hs_theme") === "dark" ||
+  (localStorage.getItem("hs_theme") === "auto" &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+if (isLightOrAuto && html.classList.contains("dark"))
+  html.classList.remove("dark");
+else if (isDarkOrAuto && html.classList.contains("light"))
+  html.classList.remove("light");
+else if (isDarkOrAuto && !html.classList.contains("dark"))
+  html.classList.add("dark");
+else if (isLightOrAuto && !html.classList.contains("light"))
+  html.classList.add("light");
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
